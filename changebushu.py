a5a7d6777428f1b89dcc50529161995c6a3f7129
wanjf <66167613+19399@users.noreply.github.com>
2023-04-03 14:46:52 +0800

    Update changebushu.py

--- changebushu.py
+++ changebushu.py
@@ -5,10 +5,10 @@ headers = {
     'User-Agent': 'Dalvik/2.1.0 (Linux; U; Android 9; MI 6 MIUI/20.6.18)'
 }
  
-user = ''
-password = ''
-# step = ''
-step = str(randint(10121, 12302))
+user = os.environ['USER_PHONE']
+password = os.environ['USER_PWD']
+    # step = str(randint(int(os.environ['STEP_MIN']), int(os.environ['STEP_MAX'])))
+step = os.environ['STEP']
  
 def get_code(location):
     code_pattern = re.compile("(?<=access=).*?(?=&)")
